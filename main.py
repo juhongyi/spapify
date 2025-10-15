bc41e39f41d525033604357e311300eebd45983a
Yi Hong-ju <to_hongju@pm.me>
2025-10-15 09:11:10 +0900

    Add functions for PoC of "Get New Releases" API

--- main.py
+++ main.py
@@ -1,5 +1,58 @@
+import os
+import base64
+
+import requests
+
+
+def poc_get_access_token() -> str:
+    client_id = os.getenv("SPTFY_CLIENT_ID")
+    client_secret = os.getenv("SPTFY_CLIENT_SECRET")
+    client_string = base64.b64encode(f"{client_id}:{client_secret}".encode()).decode()
+
+    response = requests.post(
+        "https://accounts.spotify.com/api/token",
+        headers={
+            "Authorization": f"Basic {client_string}",
+            "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data={"grant_type": "client_credentials"},
+    )
+    return response.json().get("access_token")
+
+
+def poc_get_new_releases_etag():
+    # entire_albums = []
+    etags = []
+    url = "https://api.spotify.com/v1/browse/new-releases?limit=50"  # max limit is 50
+
+    while True:
+        response = requests.get(
+            url,
+            headers={"Authorization": f"Bearer {poc_get_access_token()}"},
+        )
+        albums = response.json().get("albums")
+        # entire_albums.append(albums)
+
+        etags.append(response.headers.get("ETag"))
+
+        next_url = albums.get("next")
+        if next_url is None:
+            break
+        url = next_url
+
+    webhook_id = os.getenv("DSCRD_WEBHOOK_ID")
+    webhook_token = os.getenv("DSCRD_WEBHOOK_TOKEN")
+    webhook_url = f"https://discord.com/api/webhooks/{webhook_id}/{webhook_token}"
+
+    for etag in etags:
+        response = requests.post(
+            webhook_url,
+            json={"content": etag.strip('"')},
+        )
+
+
 def main():
-    print("Hello from spapify!")
+    poc_get_new_releases_etag()
 
 
 if __name__ == "__main__":
