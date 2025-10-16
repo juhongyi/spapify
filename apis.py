8b16c28fb3198c2a9b6f4b9352673550230b19ff
Yi Hong-ju <to_hongju@pm.me>
2025-10-16 08:39:19 +0000

    Add send_discord_alert() in apis.py

--- apis.py
+++ apis.py
@@ -53,3 +53,42 @@ def get_access_token(client_string: str) -> str:
 
     logging.error("Failed to get access_token from Spotify after max retries")
     raise ValueError("Failed to get access_token from Spotify after max retries")
+
+
+def send_discord_alert(
+    discord_webhook_id: str, discord_webhook_token: str, message: str
+) -> None:
+    """Send alert message to Discord webhook with retries.
+
+    If failed to send alert after max retries, it will just 'log' an error.
+
+    Args:
+        discord_webhook_id (str): The ID of the Discord webhook.
+        discord_webhook_token (str): The token of the Discord webhook.
+        message (str): The alert message to send.
+    """
+
+    retries = 0
+
+    while retries < MAX_RETRIES:
+        try:
+            response = requests.post(
+                f"https://discord.com/api/webhooks/{discord_webhook_id}/{discord_webhook_token}",
+                json={"content": message},
+            )
+
+            if response.status_code == 200:
+                logging.info(
+                    "Successfully sent Discord alert with message: %s", message
+                )
+                return  # Early return
+
+            logging.warning("Failed to send Discord alert with message: %s", message)
+            retries += 1
+
+            if retries < MAX_RETRIES:
+                time.sleep(2**retries)  # 2, 4, 8, 16, ...
+        except requests.RequestException as e:
+            logging.warning("RequestException when calling Discord webhook: %s", e)
+
+    logging.error("Failed to send Discord alert after max retries")
