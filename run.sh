a63809f7db6711655642d62266c27d7b9da53309
Yi Hong-ju <to_hongju@pm.me>
2025-10-16 06:03:42 +0000

    Parse "--job" argument and pass it to main.py

--- run.sh
+++ run.sh
@@ -2,5 +2,13 @@
 
 cd $HOME/spapify  # Cloned repo directory
 
+for arg in "$@"; do
+  case $arg in
+    --job=*)
+      JOB_NAME="${arg#*=}"
+      ;;
+  esac
+done
+
 . $HOME/.local/bin/env  # Add 'uv', 'dotenvx' binary to PATH
-dotenvx run -f .env -- uv run main.py
+dotenvx run -f .env -- uv run main.py --job=$JOB_NAME
