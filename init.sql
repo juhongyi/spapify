91a077164fd508a2b5039b065b7ebceba6cd9abd
Yi Hong-ju <to_hongju@pm.me>
2025-10-23 09:31:56 +0900

    Add init.sql for database Docker service init

---
+++ init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE artists (
+    id          SERIAL          PRIMARY KEY,
+    name        VARCHAR(255)    NOT NULL,
+    lastfm_url  VARCHAR(255)    NOT NULL UNIQUE
+);
+CREATE TABLE tracks (
+    id          SERIAL          PRIMARY KEY,
+    name        VARCHAR(255)    NOT NULL,
+    artist_id   INTEGER         NOT NULL REFERENCES artists(id) ON DELETE CASCADE,
+    lastfm_url  VARCHAR(255)    NOT NULL UNIQUE
+);
+CREATE TABLE chart_histories (
+    id          SERIAL          PRIMARY KEY,
+    track_id    INTEGER         NOT NULL REFERENCES tracks(id) ON DELETE CASCADE,
+    playcount   INTEGER         NOT NULL,
+    listeners   INTEGER         NOT NULL,
+    chart_date  TIMESTAMP       NOT NULL,
+    rank        SMALLINT        NOT NULL,
+    UNIQUE(track_id, chart_date)
+);
